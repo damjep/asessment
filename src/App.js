6044fa29a5003c48be37a63a9d9cb34d9378193a
Adam Jeffery <adamjeff143@gmail.com>
2024-03-16 00:53:38 +0000

    adam

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import data from "./convertcsv.json"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const table1Val = data.map(c => {
+    return (<>
+        <tr>
+          <td>{c["Index #"]}</td>
+          <td>
+            {c.Value}
+          </td>
+        </tr>     
+    </>)
+  })
+
+
+  function Tab() {
+    const Alpha = 2+28
+    const Beta = 80 / 5
+    const Charlie = 90 * 3
+
+    return (<>
+    <tr>
+      <td>Alpha</td>
+      <td>{Alpha}</td>
+    </tr>
+    <tr>
+      <td>Beta</td>
+      <td>{Beta}</td>
+    </tr>
+    <tr>
+      <td>Charlie</td>
+      <td>{Charlie}</td>
+    </tr>
+    </>)
+  }
+
+  return (<>
+  <div className="table1">
+    <h2>Table 1</h2>
+    <table>
+        <tr>
+          <th>Value</th>
+          <th>Index</th>
+        </tr>
+        {table1Val}
+    </table>
+  </div>
+    
+    <div className="tab2">
+      <h2>Table 2</h2>
+      <table>
+          <tr>
+            <th>Category</th>
+            <th>Value</th>
+          </tr>
+          < Tab />
+      </table>
     </div>
-  );
+  </>)
+    
 }
 
 export default App;
